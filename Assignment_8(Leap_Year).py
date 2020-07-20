43bf09b6178d6e445b29a89f76c1e82726c12e11
Raymond <raymondaksu@gmail.com>
2020-07-20 19:12:42 +0300

    This is Leap_Year assignment(Assignment-8)

---
+++ Assignment_8(Leap_Year).py
@@ -0,0 +1,7 @@
+def leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is not a leap year.")
+
+leap_year(2019)
