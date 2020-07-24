517b6d4e15160a8eef665bf5b14a40ea5ed23428
Raymond <raymondaksu@gmail.com>
2020-07-24 23:39:28 +0300

    Assignment of Letter Counts (Assignment-9)

---
+++ Assignment-9(Letters_Count).py
@@ -0,0 +1,18 @@
+sentence = input("Enter a sentence: ").split()
+letter_list = []
+for i in sentence:
+    for j in i:
+        if j.isalpha():
+            letter_list.append(j)
+my_dict = {}
+set_list = set(letter_list)
+for i in set_list:
+    counter = letter_list.count(i)
+    my_dict[i] = counter
+    count_of_letters.append(counter)
+
+print(my_dict)
+
+
+
+
